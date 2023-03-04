ba3d9e6b24c22b148eb8cdc13a693a8d7487b6d5
David Stromberger <Davidstromberger99@gmx.de>
2023-03-04 12:53:02 +0100

    Added command processor for organization

--- netcord.py
+++ netcord.py
@@ -1,34 +1,32 @@
-import os
+#!/usr/bin/env python3
 
+import asyncio
+import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-
 from src.config import DiscordConfig
-from src.discord_formatter import DiscordFormatter
-from src.net import ping
+from src.command_processor import CommandProcessor
 
 load_dotenv()
+
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+CONFIG_FILE = os.getenv('CONFIG_FILE')
 
+if TOKEN is None:
+    raise ValueError("DISCORD_TOKEN is not set")
+if CONFIG_FILE is None:
+    raise ValueError("CONFIG_FILE is not set")
+
+config = DiscordConfig(CONFIG_FILE)
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix='!')
-config = DiscordConfig('config/format.json')
-formatter = DiscordFormatter(config)
+processor = CommandProcessor(config)
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+async def setup(bot: commands.Bot):
+    await bot.add_cog(processor)
 
-    command = message.content.split()[0][1:]
-    if command == 'ping':
-        ip_address = message.content.split()[1]
-        reachable = ping(ip_address)
-        if reachable:
-            await message.channel.send(formatter.format_text(f"[{ip_address}] - Online"))
-        else:
-            await message.channel.send(formatter.format_text(f"[{ip_address}] - Offline"))
 
-bot.run(TOKEN)
+if __name__ == '__main__':
+    asyncio.run(setup(bot))
+    bot.run(TOKEN)
---
+++ src/command_processor.py
@@ -0,0 +1,21 @@
+from src.net import ping
+from src.discord_formatter import DiscordFormatter
+from discord.ext import commands
+
+
+class CommandProcessor(commands.Cog):
+    def __init__(self, config):
+        self.config = config
+        self.formatter = DiscordFormatter(config)
+
+    @commands.command(name="ping")
+    async def ping(self, ctx, ip_address: str):
+        reachable = ping(ip_address)
+        if reachable:
+            await ctx.send(self.formatter.format_text(f"[{ip_address}] - Online"))
+        else:
+            await ctx.send(self.formatter.format_text(f"[{ip_address}] - Offline"))
+
+    @commands.command(name="commands")
+    async def help(self, ctx):
+        await ctx.send(self.formatter.format_text("Available commands:\n- ping [IP address]\n- help"))
